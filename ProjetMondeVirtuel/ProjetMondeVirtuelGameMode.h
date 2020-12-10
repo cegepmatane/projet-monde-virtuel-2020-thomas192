e63adfc20f50977fc2de83ee26d157ce2594fc15
Thomas Saudemont <65908739+thomas192@users.noreply.github.com>
2020-12-11 00:01:45 +0100

    Pathfinding d'un cube vers le joueur

---
+++ ProjetMondeVirtuel/ProjetMondeVirtuelGameMode.h
@@ -0,0 +1,63 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "ProjetMondeVirtuelGameMode.generated.h"
+
+// Différents états de jeu
+UENUM(BlueprintType)
+enum class EEtatJeu : uint8
+{
+	EEnJeu,
+	EFini,
+	EInconnu
+};
+
+UCLASS(minimalapi)
+class AProjetMondeVirtuelGameMode : public AGameMode
+{
+	GENERATED_BODY()
+
+private:
+	/** Garder trace de l'état de jeu actuel */
+	EEtatJeu etatActuel;
+
+	/** Liste qui stocke les volumes d'apparition instanciés */
+	TArray<class AApparition*> apparitions;
+
+protected:
+	/** Taux de perte de vie du personnage */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Vie", Meta = (BlueprintProtected = "true"))
+		float tauxPerteVie;
+
+	/** La classe Widget utilisée pour le HUD */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Vie", Meta = (BlueprintProtected = "true"))
+		TSubclassOf<class UUserWidget> classeWidgetHUD;
+
+	/** L'instance du HUD */
+	UPROPERTY()
+		class UUserWidget* widgetActuel;
+
+private:
+	/** Gère les appels de fonction qui dépendent du changement d'état de jeu */
+	void gererNouvelEtat(EEtatJeu nouvelEtat);
+
+public:
+	AProjetMondeVirtuelGameMode();
+
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void BeginPlay() override;
+
+	/** Retourne l'état de jeu actuel */
+	UFUNCTION(BlueprintPure, Category = "Cube")
+		EEtatJeu getEtatActuel() const;
+
+	/** Définit l'état de jeu actuel */
+	void setEtatActuel(EEtatJeu nouvelEtat);
+};
+
+
+
